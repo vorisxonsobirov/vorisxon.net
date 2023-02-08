92f4573ad66c4dcd8cd1c69b44b53523573ddf30
vorisxonsobirov <106791725+vorisxonsobirov@users.noreply.github.com>
2023-02-08 08:08:44 +0500

    Add files via upload

---
+++ portfolio.js/js/scripts.js
@@ -0,0 +1,81 @@
+/*!
+* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+ var ism = prompt ("what is your name ");
+alert ("name please");
+
+ var ism = prompt ("name please");
+ if (ism == "") {
+     alert("you must enter your name")
+ } 
+ var yosh = prompt ("How old are you");
+ if (yosh > 18) {
+     alert ("You are over 18 years old!")
+ } else {
+    alert ("you are not 18 years old!")
+ }
+ var email = prompt ("please enter your e-mail");
+ if (email == "") {
+    alert("please enter your e-mail")
+} else { 
+       alert ("You didn't write an email")
+     }
+ alert ("hello " +  ism + " welcome to my website");
+//  var email = prompt ("please enter your e-mail");
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
